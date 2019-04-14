84aba202f96ef1759b82e29b3a8fcd23254fd4e3
mich101mich <mich101mich@gmail.com>
2019-04-14 20:22:53 +0200

    day 6.1

--- src/main.rs
+++ src/main.rs
@@ -10,34 +10,40 @@ macro_rules! pv {
 }
 
 fn main() {
-    let input = include_str!("input/day_05.txt");
+    let input = include_str!("input/day_06.txt");
     /*
-    let input = r#"0
-3
-0
-1
--3"#;
+    let input = "0\t2\t7\t0";
     // */
-    let mut lines = input
-        .lines()
+    let mut banks = input
+        .split('\t')
         .map(|line| i32::from_str(line).unwrap_or_else(|_| panic!("{}", line)))
         .to_vec();
 
-    let mut current: i32 = 0;
-    let mut steps = 0;
+    let mut seen = HashSet::new();
+    let mut iterations = 0;
+    let len = banks.len();
 
-    while current >= 0 && current < lines.len() as i32 {
-        let val = lines[current as usize];
-        if val >= 3 {
-            lines[current as usize] -= 1;
-        } else {
-            lines[current as usize] += 1;
+    loop {
+        let mut mi = 0;
+        for i in 1..len {
+            if banks[i] > banks[mi] {
+                mi = i;
+            }
         }
-        current += val;
-        steps += 1;
+        let count = banks[mi];
+        banks[mi] = 0;
+        for di in 0..count {
+            banks[(mi + 1 + di as usize) % len] += 1;
+        }
+
+        iterations += 1;
+
+        if !seen.insert(format!("{:?}", banks)) {
+            break;
+        }
     }
 
-    pv!(steps);
+    pv!(iterations);
 }
 
 trait IterExt<T> {
