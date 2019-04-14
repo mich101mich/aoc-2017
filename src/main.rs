847bd06613c7973934bcc4d4fb8bbff4b712ff72
mich101mich <mich101mich@gmail.com>
2019-04-14 20:59:19 +0200

    day 7.2

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ gyxo (61)
 cntj (57)";
     // */
     let mut parents = HashMap::new();
+    let mut children = HashMap::new();
     let mut disks = HashMap::new();
 
     for line in input.lines() {
@@ -39,7 +40,7 @@ cntj (57)";
 
         if let Some(_) = iter.next() {
             // arrow
-            let children = iter
+            let child_disks = iter
                 .map(|c| {
                     if c.ends_with(',') {
                         &c[..c.len() - 1]
@@ -49,9 +50,11 @@ cntj (57)";
                 })
                 .to_vec();
 
-            for child in children {
+            for &child in &child_disks {
                 parents.insert(child, name);
             }
+
+            children.insert(name, child_disks);
         }
     }
     let mut root = *parents.keys().next().unwrap();
@@ -59,7 +62,29 @@ cntj (57)";
         root = parents[root];
     }
 
-    pv!(root);
+    fn get_weight(name: &str, d: &HashMap<&str, i32>, c: &HashMap<&str, Vec<&str>>) -> i32 {
+        let mut me = d[name];
+        if let Some(children) = c.get(name) {
+            me += children
+                .iter()
+                .map(|&child| get_weight(child, d, c))
+                .sum::<i32>();
+        }
+        me
+    };
+
+    for (disk, my_children) in children.iter() {
+        let child_weights = my_children
+            .iter()
+            .map(|&child| get_weight(child, &disks, &children))
+            .to_vec();
+        if child_weights.iter().any(|w| *w != child_weights[0]) {
+            pv!(disk);
+            for c in 0..my_children.len() {
+                println!("{}: {} ({})", my_children[c], child_weights[c], disks[my_children[c]]);
+            }
+        }
+    }
 }
 
 trait IterExt<T> {
