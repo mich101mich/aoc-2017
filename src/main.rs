82a1e276e1c1c35f49d712762546cb3616983e21
mich101mich <mich101mich@gmail.com>
2019-04-14 18:30:35 +0200

    day 2.2

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ macro_rules! pv {
 fn main() {
     let input = include_str!("input/day_02.txt");
     /*
-    let input = r#"5	1	9	5
-7	5	3
-2	4	6	8"#;
-    // */
+        let input = r#"5	1	9	5
+    7	5	3
+    2	4	6	8"#;
+        // */
 
     let nums = input
         .lines()
@@ -24,10 +24,22 @@ fn main() {
 
     let sum: i32 = nums
         .iter()
-        .map(|row| row.iter().max().unwrap() - row.iter().min().unwrap())
+        .map(|row| {
+            let mut sum = 0;
+            for (i, &n) in row.iter().enumerate() {
+                for &n2 in row.iter().skip(i + 1) {
+                    if n % n2 == 0 {
+                        sum += n / n2;
+                    } else if n2 % n == 0 {
+                        sum += n2 / n;
+                    }
+                }
+            }
+            sum
+        })
         .sum();
 
-    let sum = pv!(sum);
+    pv!(sum);
 }
 
 trait IterExt<T> {
